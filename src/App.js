a2cce4b5d49290f99d49ad60f103b3fd1d903421
abdulloh19 <parij4320@gmail.com>
2023-04-09 16:51:51 +0500

    added login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    active: true,
+  };
+
+  changeActive = () => {
+    this.setState = {
+      active: !this.state.active,
+    };
+  };
+
+  render() {
+    return (
+      <div className="container mt-4">
+        {this.state.active ? (
+          <div>
+            <h3>Welcome</h3>
+            <button
+              className="btn btn-outline-dark"
+              onClick={this.changeActive}
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div>
+            <h3>Login</h3>
+            <button
+              className="btn btn-outline-danger"
+              onClick={this.changeActive}
+            >
+              Login
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
